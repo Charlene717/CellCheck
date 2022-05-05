9fd66dc12b48eb715025a32f8eb4c7fad50d53aa
CharleneMSI <q2330416@gmail.com>
2022-05-06 01:25:35 +0800

    Add TestID

--- Fun_Draw_ConfuMax.R
+++ Fun_Draw_ConfuMax.R
@@ -1,14 +1,14 @@
 ## https://stackoverflow.com/questions/23891140/r-how-to-visualize-confusion-matrix-using-the-caret-package
 ## @Cybernetic  ## https://stackoverflow.com/users/1639594/cybernetic
 
-draw_confusion_matrix <- function(cm) {
-  
+draw_confusion_matrix <- function(cm,TestID = "Predict1") {
+
   layout(matrix(c(1,1,2)))
   par(mar=c(2,2,2,2))
   plot(c(100, 345), c(300, 450), type = "n", xlab="", ylab="", xaxt='n', yaxt='n')
-  title('CONFUSION MATRIX', cex.main=2)
-  
-  # create the matrix 
+  title(paste0('CONFUSION MATRIX'," (",TestID,") "), cex.main=2.1)
+
+  # create the matrix
   rect(150, 430, 240, 370, col='#3F97D0')
   text(195, 435, 'Class1', cex=1.2)
   rect(250, 430, 340, 370, col='#F7AD50')
@@ -19,15 +19,15 @@ draw_confusion_matrix <- function(cm) {
   rect(250, 305, 340, 365, col='#3F97D0')
   text(140, 400, 'Class1', cex=1.2, srt=90)
   text(140, 335, 'Class2', cex=1.2, srt=90)
-  
-  # add in the cm results 
+
+  # add in the cm results
   res <- as.numeric(cm$table)
   text(195, 400, res[1], cex=1.6, font=2, col='white')
   text(195, 335, res[2], cex=1.6, font=2, col='white')
   text(295, 400, res[3], cex=1.6, font=2, col='white')
   text(295, 335, res[4], cex=1.6, font=2, col='white')
-  
-  # add in the specifics 
+
+  # add in the specifics
   plot(c(100, 0), c(100, 0), type = "n", xlab="", ylab="", main = "DETAILS", xaxt='n', yaxt='n')
   text(10, 85, names(cm$byClass[1]), cex=1.2, font=2)
   text(10, 70, round(as.numeric(cm$byClass[1]), 3), cex=1.2)
@@ -39,10 +39,10 @@ draw_confusion_matrix <- function(cm) {
   text(70, 70, round(as.numeric(cm$byClass[6]), 3), cex=1.2)
   text(90, 85, names(cm$byClass[7]), cex=1.2, font=2)
   text(90, 70, round(as.numeric(cm$byClass[7]), 3), cex=1.2)
-  
-  # add in the accuracy information 
+
+  # add in the accuracy information
   text(30, 35, names(cm$overall[1]), cex=1.5, font=2)
   text(30, 20, round(as.numeric(cm$overall[1]), 3), cex=1.4)
   text(70, 35, names(cm$overall[2]), cex=1.5, font=2)
   text(70, 20, round(as.numeric(cm$overall[2]), 3), cex=1.4)
-}  
+}
