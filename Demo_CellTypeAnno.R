c414f12ba18eaac64e0cdaaeeb7822fc9b8f2362
CharleneMSI <q2330416@gmail.com>
2022-05-06 00:49:16 +0800

    Export PDF files of full version of CM

--- Demo_CellTypeAnno.R
+++ Demo_CellTypeAnno.R
@@ -1,7 +1,14 @@
 ##### To Do List ######
 ## Discrete data: Binary data
   # - [ ] Confusion matrix (Simple version)
+  #   - [ ] Basic version
+  #   - [ ] Formula
+  #   - [ ] Beautify
   # - [ ] Confusion matrix (Full version)
+  #   - [x] Basic version
+  #   - [ ] Formula
+  #   - [ ] Beautify
+
   # - [x] Annotation table
   # - [x] Compare different conditions
 
@@ -38,54 +45,54 @@
   dir.create(Save.Path)
 
 
-
 ##### Load simulation datafrme #####
   load("D:/Dropbox/##_GitHub/##_CAESAR/CellCheck/Create_simulation_datafrme.RData")
 
 
 ##### Binary data #####
-  ##### Compare Multigroup #####
-      cm.lt <- list()
+  #### calculate the confusion matrix ####
+    library(caret)
 
-      for (i in 1:(ncol(Check_Bi.df)-1)) {
-        cm.lt[[i]] <- confusionMatrix(data = Check_Bi.df[,1] %>% as.factor(),
-                              reference = Check_Bi.df[,1+i] %>% as.factor())
-        names(cm.lt)[[i]] <- colnames(Check_Bi.df)[i+1]
+    cm.lt <- list()
 
+    for (i in 1:(ncol(Check_Bi.df)-1)) {
+      cm.lt[[i]] <- confusionMatrix(data = Check_Bi.df[,1] %>% as.factor(),
+                            reference = Check_Bi.df[,1+i] %>% as.factor())
+      names(cm.lt)[[i]] <- colnames(Check_Bi.df)[i+1]
+
+    }
+    rm(i)
+
+    # cm <- confusionMatrix(data = Check_Bi.df$Actual %>% as.factor(),
+    #                       reference = Check_Bi.df$Predict1 %>% as.factor())
+
+  ####  Plot Result by Bar chart of Metrics ####
+    ## Result dataframe
+    Results.df <- Summary_CM(cm.lt, Anno.df)
+
+    # Ref(Bar Chart): https://officeguide.cc/r-ggplot2-bar-plot-tutorial-examples/
+    # Ref(Color): http://rstudio-pubs-static.s3.amazonaws.com/5312_98fc1aba2d5740dd849a5ab797cc2c8d.html
+
+    ## Plot by group
+    p <- Plot_CMBar(Results.df, Metrics = "Kappa")
+    p
+
+    Metrics.set <- colnames(Results.df)[2:(ncol(Results.df)-(ncol(Anno.df)-1))]
+
+    pdf(file = paste0(Save.Path,"/",ProjectName,"_MetricsBar.pdf"),
+      width = 7,  height = 7)
+      for (i in 1:length(Metrics.set)) {
+          p <- Plot_CMBar(Results.df, Metrics = Metrics.set[i])
+          p
       }
-      rm(i)
+    graphics.off()
+    rm(p,i)
 
-
-      ## Result dataframe
-        Results.df <- Summary_CM(cm.lt, Anno.df)
-
-      ## Plot Result
-        # Ref(Bar Chart): https://officeguide.cc/r-ggplot2-bar-plot-tutorial-examples/
-        # Ref(Color): http://rstudio-pubs-static.s3.amazonaws.com/5312_98fc1aba2d5740dd849a5ab797cc2c8d.html
-
-        ## Plot by group
-        p <- Plot_CMBar(Results.df, Metrics = "Kappa")
-        p
-
-        Metrics.set <- colnames(Results.df)[2:(ncol(Results.df)-(ncol(Anno.df)-1))]
-
-        pdf(file = paste0(Save.Path,"/",ProjectName,"_MetricsBar.pdf"),
-          width = 7,  height = 7)
-          for (i in 1:length(Metrics.set)) {
-              p <- Plot_CMBar(Results.df, Metrics = Metrics.set[i])
-              p
-          }
-        graphics.off()
-        rm(p)
-
-  ##### Confusion matrix #####
-      #### calculate the confusion matrix ####
-        library(caret)
-        # cm <- confusionMatrix(data = Check_Bi.df$Actual %>% as.factor(),
-        #                       reference = Check_Bi.df$Predict1 %>% as.factor())
+  ##### Plot Confusion matrix #####
+      ## Extract the confusion matrix
         cm <- cm.lt[["Predict1"]]
 
-      #### Draw Confusion Matrix ####
+      ## Draw Confusion matrix
         source("Fun_Draw_ConfuMax.R")
         draw_confusion_matrix(cm)
         Draw_CM(cm)
@@ -94,9 +101,13 @@
           file = paste0(Save.Path,"/",ProjectName,"_ConfuMax.pdf"),
           width = 17,  height = 12
         )
-          Draw_CM(cm)
+          for (i in 1:length(cm.lt)) {
 
-        dev.off()
+            Draw_CM(cm.lt[[i]],names(cm.lt[i]))
+          }
+        #dev.off()
+        graphics.off()
+        rm(i)
 
 
 #########################################################################################################
