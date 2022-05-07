c1b45066f10a570fe3a732a15243de806290eda0
CharleneMSI <q2330416@gmail.com>
2022-05-07 20:43:43 +0800

    Add measure accuracy

--- Demo_CellTypeAnno.R
+++ Demo_CellTypeAnno.R
@@ -42,13 +42,15 @@
   library(tidyverse)
   library(caret) # Confusion matrix
   library(cvms) # Confusion matrix for Multi-Class Classification
+  library(DescTools) # Measures of Accuracy
 
 ##### Function setting #####
   ## Call function
   source("Fun_Draw_ConfuMax.R")
   source("FUN_Draw_ConfuMatrix.R")
-  source("FUN_Summary_CM.R")
+  source("FUN_Summarize_BiCM.R")
   source("FUN_Plot_CMBar.R")
+  source("FUN_Measure_Accuracy.R")
 
 ##### Current path and new folder setting*  #####
   ProjectName = "CC"
@@ -68,20 +70,20 @@
 
     cm.lt <- list()
 
-    for (i in 1:(ncol(Check_Bi.df)-1)) {
-      cm.lt[[i]] <- confusionMatrix(data = Check_Bi.df[,1] %>% as.factor(),
-                            reference = Check_Bi.df[,1+i] %>% as.factor())
-      names(cm.lt)[[i]] <- colnames(Check_Bi.df)[i+1]
+    for (i in 1:(ncol(Simu_Bi.df)-1)) {
+      cm.lt[[i]] <- confusionMatrix(data = Simu_Bi.df[,1] %>% as.factor(),
+                            reference = Simu_Bi.df[,1+i] %>% as.factor())
+      names(cm.lt)[[i]] <- colnames(Simu_Bi.df)[i+1]
 
     }
     rm(i)
 
-    # cm <- confusionMatrix(data = Check_Bi.df$Actual %>% as.factor(),
-    #                       reference = Check_Bi.df$Predict1 %>% as.factor())
+    # cm <- confusionMatrix(data = Simu_Bi.df$Actual %>% as.factor(),
+    #                       reference = Simu_Bi.df$Predict1 %>% as.factor())
 
   ####  Plot Result by Bar chart of Metrics ####
     ## Result dataframe
-    Results_Bi.df <- Summary_CM(cm.lt, Anno.df)
+    Results_Bi.df <- Summarize_BiCM (cm.lt, Anno.df)
 
     ## Plot by group
     p1 <- Plot_CMBar(Results_Bi.df, Metrics = "Accuracy")
@@ -90,7 +92,7 @@
 
     Metrics_Bi.set <- colnames(Results_Bi.df)[2:(ncol(Results_Bi.df)-(ncol(Anno.df)-1))]
 
-    pdf(file = paste0(Save.Path,"/",ProjectName,"_MetricsBar_Bi.pdf"),
+    pdf(file = paste0(Save.Path,"/",ProjectName,"_Bi_MetricsBar.pdf"),
       width = 7,  height = 7)
       for (i in 1:length(Metrics_Bi.set)) {
           p <- Plot_CMBar(Results_Bi.df, Metrics = Metrics_Bi.set[i])
@@ -111,7 +113,7 @@
 
         ## Full version
           pdf(
-            file = paste0(Save.Path,"/",ProjectName,"_ConfuMax_Bi.pdf"),
+            file = paste0(Save.Path,"/",ProjectName,"_Bi_ConfuMax.pdf"),
             width = 17,  height = 12
           )
             for (i in 1:length(cm.lt)) {
@@ -124,7 +126,7 @@
 
         ## Simple version
           pdf(
-            file = paste0(Save.Path,"/",ProjectName,"_ConfuMaxSimp_Bi.pdf"),
+            file = paste0(Save.Path,"/",ProjectName,"_Bi_ConfuMaxSimp.pdf"),
             width = 10,  height = 7
           )
           for (i in 1:length(cm.lt)) {
@@ -138,28 +140,28 @@
 
 #########################################################################################################
   ##### Misclassification rate (error rate) #####
-    # Check_DisMult.df$Correctness1 <- ""
+    # Simu_DisMult.df$Correctness1 <- ""
 
-    Check_DisMult_Res.df <- data.frame(matrix("",0,(ncol(Check_DisMult.df)-1)))
-    colnames(Check_DisMult_Res.df) <- colnames(Check_DisMult.df)[2:ncol(Check_DisMult.df)]
+    Simu_DisMult_Res.df <- data.frame(matrix("",0,(ncol(Simu_DisMult.df)-1)))
+    colnames(Simu_DisMult_Res.df) <- colnames(Simu_DisMult.df)[2:ncol(Simu_DisMult.df)]
 
     # MissRate: Misclassification rate
       cm_DisMult.lt <- list()
-      for (j in 1:(ncol(Check_DisMult.df)-1)) {
+      for (j in 1:(ncol(Simu_DisMult.df)-1)) {
 
-        for (i in 1:nrow(Check_DisMult.df)) {
-          if(Check_DisMult.df[i,1] == Check_DisMult.df[i,j+1]){
-            Check_DisMult_Res.df[i,j] = 0
+        for (i in 1:nrow(Simu_DisMult.df)) {
+          if(Simu_DisMult.df[i,1] == Simu_DisMult.df[i,j+1]){
+            Simu_DisMult_Res.df[i,j] = 0
           }else{
-            Check_DisMult_Res.df[i,j] = 1
+            Simu_DisMult_Res.df[i,j] = 1
           }
         }
-        cm_DisMult.lt[j] <- sum(Check_DisMult_Res.df[,j] == 1)/nrow(Check_DisMult_Res.df)
-        names(cm_DisMult.lt)[j] <- colnames(Check_DisMult_Res.df)[j]
+        cm_DisMult.lt[j] <- sum(Simu_DisMult_Res.df[,j] == 1)/nrow(Simu_DisMult_Res.df)
+        names(cm_DisMult.lt)[j] <- colnames(Simu_DisMult_Res.df)[j]
       }
 
-      rm(i,j,Check_DisMult_Res.df)
-      Results_DisMult.df <- data.frame(TestID = colnames(Check_DisMult.df)[2:ncol(Check_DisMult.df)],
+      rm(i,j,Simu_DisMult_Res.df)
+      Results_DisMult.df <- data.frame(TestID = colnames(Simu_DisMult.df)[2:ncol(Simu_DisMult.df)],
                                        Misclass = unlist(cm_DisMult.lt),
                                        Accuracy = 1-unlist(cm_DisMult.lt))
 
@@ -174,7 +176,7 @@
 
       Metrics_DisMult.set <- colnames(Results_DisMult.df)[2:(ncol(Results_DisMult.df)-(ncol(Anno.df)-1))]
 
-      pdf(file = paste0(Save.Path,"/",ProjectName,"_MetricsBar_DisMult.pdf"),
+      pdf(file = paste0(Save.Path,"/",ProjectName,"_DisMult__MetricsBar.pdf"),
           width = 7,  height = 7)
       for (i in 1:length(Metrics_DisMult.set)) {
         p <- Plot_CMBar(Results_DisMult.df, Metrics = Metrics_DisMult.set[i])
@@ -190,8 +192,8 @@
       # install.packages("cvms")
       library(cvms)
       ##
-      conf_mat <- confusion_matrix(targets = Check_DisMult.df$Actual,
-                                   predictions = Check_DisMult.df$Predict1)
+      conf_mat <- confusion_matrix(targets = Simu_DisMult.df$Actual,
+                                   predictions = Simu_DisMult.df$Predict1)
 
       conf_mat
 
@@ -204,8 +206,8 @@
                  axis.title.y = element_text(face="bold", color="#3d3d3d", size=18))
 
       ##
-      conf_mat2 <- confusion_matrix(targets = Check_DisMult.df$Actual,
-                                   predictions = Check_DisMult.df[,3])
+      conf_mat2 <- confusion_matrix(targets = Simu_DisMult.df$Actual,
+                                   predictions = Simu_DisMult.df[,3])
 
       conf_mat2
 
@@ -216,17 +218,17 @@
       #### calculate the confusion matrix ####
       conf_mat.lt <- list()
 
-      for (i in 1:(ncol(Check_DisMult.df)-1)) {
-        conf_mat.lt[[i]] <- confusion_matrix(targets = Check_DisMult.df[,1] %>% as.factor(),
-                                             predictions = Check_DisMult.df[,1+i] %>% as.factor())
-        names(conf_mat.lt)[[i]] <- colnames(Check_DisMult.df)[i+1]
+      for (i in 1:(ncol(Simu_DisMult.df)-1)) {
+        conf_mat.lt[[i]] <- confusion_matrix(targets = Simu_DisMult.df[,1] %>% as.factor(),
+                                             predictions = Simu_DisMult.df[,1+i] %>% as.factor())
+        names(conf_mat.lt)[[i]] <- colnames(Simu_DisMult.df)[i+1]
 
       }
       rm(i)
 
       #### Export PDF ####
       pdf(
-        file = paste0(Save.Path,"/",ProjectName,"_ConfuMax_DisMult.pdf"),
+        file = paste0(Save.Path,"/",ProjectName,"_DisMult_ConfuMax.pdf"),
         width = 10,  height = 10
       )
         for (i in 1:length(conf_mat.lt)) {
@@ -243,8 +245,30 @@
         }
 
       dev.off()
+      rm(p1,p2)
+
 
 #########################################################################################################
-    # RMSD: https://www.rdocumentation.org/packages/DescTools/versions/0.99.44/topics/Measures%20of%20Accuracy
-    # RMSD: https://www.rdocumentation.org/packages/CDM/versions/7.5-15/topics/IRT.RMSD
+      # install.packages("DescTools")
+      library(DescTools)
+      source("FUN_Measure_Accuracy.R")
+
+      # MA.df <- Measure_Accuracy(Simu_Conti.df$Actual,Simu_Conti.df$Predict2)
+
+      #### Create Measure accuracy dataframe (MA.df)
+      for (i in 1:(ncol(Simu_Conti.df)-1)) {
+        if(i==1){
+          MA.df <- Measure_Accuracy(Simu_Conti.df[,1],
+                                    Simu_Conti.df[,1+i])
+          row.names(MA.df) <- colnames(Simu_Conti.df)[i+1]
+        }else{
+          MA_New.df <- Measure_Accuracy(Simu_Conti.df[,1],
+                                    Simu_Conti.df[,1+i])
+          row.names(MA_New.df) <- colnames(Simu_Conti.df)[i+1]
+          MA.df <- rbind(MA.df, MA_New.df)
+
+        }
+
+      }
+      rm(i)
 
