e7a10b89b887b00159f823f4b2ff42d42071bc5b
CharleneMSI <q2330416@gmail.com>
2022-05-08 00:56:45 +0800

    Rename the parameters

--- FUN_CC_BarPlot.R
+++ FUN_CC_BarPlot.R
@@ -2,25 +2,25 @@
 # Ref(Color): http://rstudio-pubs-static.s3.amazonaws.com/5312_98fc1aba2d5740dd849a5ab797cc2c8d.html
 
 
-CC_BarPlot <- function(Results.df, MetricsX = "Type", MetricsY = "Accuracy", MetricsG = "Tool") {
+CC_BarPlot <- function(Results.df, XValue = "Type", Metrics = "Accuracy", Group = "Tool") {
 
-  metricsX = which(colnames(Results.df) == MetricsX)
-  metricsY = which(colnames(Results.df) == MetricsY)
-  metricsG = which(colnames(Results.df) == MetricsG)
+  Xvalue = which(colnames(Results.df) == XValue)
+  metrics = which(colnames(Results.df) == Metrics)
+  group = which(colnames(Results.df) == Group)
 
   ## Plot by group
-  p <- ggplot(data = Results.df, aes(x = Results.df[,metricsX], y = Results.df[,metricsY],
-                                     fill = Results.df[,metricsG]))+
+  p <- ggplot(data = Results.df, aes(x = Results.df[,Xvalue], y = Results.df[,metrics],
+                                     fill = Results.df[,group]))+
     geom_bar(stat = "identity", position = position_dodge(), color="black",lwd=0.7)+
-    theme_bw()+theme(panel.grid=element_blank(),panel.border=element_blank(),axis.line=element_line(size=1,colour="black")) # White background and remove grid
+    theme_bw()+theme(panel.grid=element_blank(),panel.border=element_blank(),axis.line=element_line(size=0,colour="black")) # White background and remove grid
 
   p2 <- p + scale_fill_brewer(palette = "Spectral")+ # scale_fill_manual(values = c("#999999", "#E69F00", "#56B4E9"))
-        labs(#title = colnames(Results.df)[metricsY],  # Change title in ggplot
-          #x = colnames(Results.df)[1],      # Change title of x axis in ggplot
-          y = colnames(Results.df)[metricsY],       # Change title of y axis in ggplot
-          fill= colnames(Results.df)[metricsG]     # Change legend title in ggplot
+        labs(#title = colnames(Results.df)[metrics],  # Change title in ggplot
+          x = colnames(Results.df)[Xvalue],      # Change title of x axis in ggplot
+          y = colnames(Results.df)[metrics],       # Change title of y axis in ggplot
+          fill= colnames(Results.df)[group]     # Change legend title in ggplot
         )+
-        theme(panel.border = element_rect(fill=NA,color="black", size= 2.5, linetype="solid"))+ # Outline
+        theme(panel.border = element_rect(fill=NA,color="black", size= 2, linetype="solid"))+ # Outline
         theme(axis.text.x = element_text(color="black",face="bold",  size = 17,angle = 45, hjust = 1, vjust = .99), # Change the size along the x axis
               axis.text.y = element_text(color="black",face="bold",size = 17), # Change the size along the y axis
 
