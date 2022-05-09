4558da0c7b5cb987a4f405e833f65c0db15c116d
CharleneMSI <q2330416@gmail.com>
2022-05-09 13:03:18 +0800

    Ori

---
+++ Version_information.R
@@ -0,0 +1,57 @@
+## version
+##### Version information ######
+  # platform       x86_64-w64-mingw32
+  # arch           x86_64
+  # os             mingw32
+  # system         x86_64, mingw32
+  # status
+  # major          4
+  # minor          1.1
+  # year           2021
+  # month          08
+  # day            10
+  # svn rev        80725
+  # language       R
+  # version.string R version 4.1.1 (2021-08-10)
+  # nickname       Kick Things
+
+## sessionInfo()
+##### R Package Version Information #####
+  # R version 4.1.1 (2021-08-10)
+  # Platform: x86_64-w64-mingw32/x64 (64-bit)
+  # Running under: Windows 10 x64 (build 19044)
+  #
+  # Matrix products: default
+  #
+  # locale:
+  #   [1] LC_COLLATE=Chinese (Traditional)_Taiwan.950  LC_CTYPE=Chinese (Traditional)_Taiwan.950
+  # [3] LC_MONETARY=Chinese (Traditional)_Taiwan.950 LC_NUMERIC=C
+  # [5] LC_TIME=Chinese (Traditional)_Taiwan.950
+  #
+  # attached base packages:
+  #   [1] stats     graphics  grDevices utils     datasets  methods   base
+  #
+  # other attached packages:
+  #   [1] DescTools_0.99.44 cvms_1.3.3        caret_6.0-92      lattice_0.20-44   forcats_0.5.1
+  # [6] stringr_1.4.0     dplyr_1.0.8       purrr_0.3.4       readr_2.1.2       tidyr_1.2.0
+  # [11] tibble_3.1.6      ggplot2_3.3.5     tidyverse_1.3.1
+  #
+  # loaded via a namespace (and not attached):
+  #   [1] nlme_3.1-153         fs_1.5.2             lubridate_1.8.0      RColorBrewer_1.1-3   httr_1.4.2
+  # [6] tools_4.1.1          backports_1.4.1      utf8_1.2.2           R6_2.5.1             rpart_4.1-15
+  # [11] DBI_1.1.2            colorspace_2.0-3     nnet_7.3-16          withr_2.5.0          tidyselect_1.1.2
+  # [16] Exact_3.1            compiler_4.1.1       cli_3.1.0            rvest_1.0.2          expm_0.999-6
+  # [21] xml2_1.3.3           labeling_0.4.2       checkmate_2.0.0      scales_1.1.1         mvtnorm_1.1-3
+  # [26] proxy_0.4-26         digest_0.6.29        pkgconfig_2.0.3      parallelly_1.31.0    dbplyr_2.1.1
+  # [31] rlang_1.0.1          readxl_1.4.0         rstudioapi_0.13      farver_2.1.0         generics_0.1.2
+  # [36] jsonlite_1.8.0       ModelMetrics_1.2.2.2 magrittr_2.0.1       Matrix_1.3-4         Rcpp_1.0.8
+  # [41] munsell_0.5.0        fansi_1.0.2          ggnewscale_0.4.7     lifecycle_1.0.1      stringi_1.7.6
+  # [46] pROC_1.18.0          rootSolve_1.8.2.3    MASS_7.3-54          plyr_1.8.6           recipes_0.2.0
+  # [51] grid_4.1.1           parallel_4.1.1       listenv_0.8.0        crayon_1.5.1         lmom_2.8
+  # [56] haven_2.4.3          splines_4.1.1        hms_1.1.1            pillar_1.7.0         boot_1.3-28
+  # [61] gld_2.6.4            future.apply_1.8.1   reshape2_1.4.4       codetools_0.2-18     stats4_4.1.1
+  # [66] reprex_2.0.1         glue_1.6.0           data.table_1.14.2    modelr_0.1.8         vctrs_0.3.8
+  # [71] tzdb_0.3.0           foreach_1.5.2        cellranger_1.1.0     gtable_0.3.0         future_1.24.0
+  # [76] assertthat_0.2.1     gower_1.0.0          prodlim_2019.11.13   broom_0.7.12         e1071_1.7-9
+  # [81] class_7.3-19         survival_3.2-13      timeDate_3043.102    iterators_1.0.14     hardhat_0.2.0
+  # [86] lava_1.6.10          globals_0.14.0       ellipsis_0.3.2       ipred_0.9-12
